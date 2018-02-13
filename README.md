1d651686307a484db9250a02d1382293cc9e9ce8
Jeconias Santos <jeconiass2009@hotmail.com>
2018-02-13 14:19:59 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# crud
-Um CRUD para otimizar suas consultas.
+# Classe CRUD
+
+Se você quer reduzir os números de linhas e evitar muitas querys e conexões com o banco de dados você pode utilizar essa classe CRUD para facilitar sua vida.
+
+   - Fácil de usar
+   - Mini sistema de log
+   - Reaproveitar conexão com o Banco de dados
+
+# Conhecendo a Classe
+Aqui irei descrever todo o funcionamento da classe e como utilizar todos os métodos que contém disponíveis no momento.
+
+### Como eu posso inicializar a classe?
+Você pode iniciar a classe de 3 maneiras distintas, quem decide é você! ;)
+
+#### Modo 1:
+Você pode utilizar sua classe de conexão com o banco de dados e passar a conexão atual para o CRUD.
+
+    $pdo = 'Sua conexão com o banco de dados';
+    $crud = new Crud($pdo);
+
+#### Modo 2:
+Você pode utilizar **define** ou array.
+
+    define('CONFIG', ['localhost', 'teste', 'root', '4514' ]);
+    $crud = new Crud(null, CONFIG);
+    ------------------------------------------------------------------------------
+    $config = array('localhost', 'teste', 'root', '4514');
+    $crud = new Crud(null, $config);
+
+#### Modo 3:
+Se você não quiser utilizar **define** ou **array**, utilize os métodos internos do CRUD.  
+
+    $crud = new Crud();
+    $crud->setDBHost('localhost');
+    $crud->setDBName('teste');
+    $crud->setDBUser('root');
+    $crud->setDBPass('4514');
+    $crud->run();
+
+- Se você utilizar o modo 3 você deve chamar o `$crud->run()` para carregar a conexão com o banco de dados.
+
+No modo 2 e 3 você pode checar se a conexão com o banco de dados foi iniciada, apenas chame `$crud->getLog()`. Você deverá receber algo como:
+
+    Conexão com o banco de dados:
+    Inicializada | ::1 | 11-02-2018 21:34:14
+
+## Métodos CRUD
+Agora que sabemos como iniciar a conexão com o banco de dados vamos aprender a utilizar os métodos CRUD.
+
+### Criando registros
+A criação de registros com essa classe é bastante fácil, basta utilizar `$crud->setInserir()`.
+
+    $crud->setInserir('usuario', array('nome' => 'Jeconias', 'email' => 'jeconiass2009@hotmail.com', 'senha' => '12'));
+
+- Primeiro parâmetro: Insira o nome da sua tabela;
+- Segundo parâmetro: Insira uma array com as chaves iguais a coluna da sua tabela;
+- Terceiro parâmetro: Você pode criptografar o valor passando o nome da chave que corresponde ao valor desejado. Padrão: senh;
+- Você pode capturar o número de linhas afetadas utilizando `$crud->getSelect()`.
+
+### Selecionando registros
+
+    $crud->setSelect('usuario', array('nome', 'email'), array('email' => 'jeconiass2009@hotmail.com'), null, array('nome' => 'ASC'));
+
+- Primeiro parâmetro: Nome da tabela;
+- Segundo parâmetro: Uma array com os valores que deseja receber. Use `array(*)` para tudo;
+- Terceiro parâmetro: Uma array que faz papel de **where**, ou seja, no exemplo a cima eu irei receber todos os registros que tenha o email correspondente. Você pode utilizar mais de um valor, ex: `array('nome' => Jeconias, 'Cidade' => 'Natal')`;
+- Quarto parâmetro: Utilize um valor inteiro para definir o limite de registros selecionados;
+- Quinto parâmetro: Uma array para classificar o conjunto de resultados em ordem ascendente ou decrescente. Use **DESC** ou **ASC**.
+
+### Atualizando registros
+
+    $crud->setUpdate('usuario', array('email' => 'joão@hotmail.com', 'nome' => 'João'), array('id' => 5));
+
+- Primeiro parâmetro: Nome da tabela;
+- Segundo parâmetro: Uma array com os valores que deseja atualizar.
+- Terceiro parâmetro: Defina qual registro irá atualizar. **where**.
+
+### Deletando registros
+
+    $crud->setDelete('usuario', array('id' => 3));
+
+- Primeiro parâmetro: Nome da tabela;
+- Segundo parâmetro: Uma array com os valores que deseja remover.
+- Obs: Se você não passar o segundo parâmetro, **TUDO** será apagado da tabela.
+
+### Log
+
+Use `$crud->getLog()` para receber o último logo gerado.
+
+## Changelog
+
+Vazio, por enquanto!
+
+## Sobre
+- Autor: Jeconias Santos
+- Licença: [MIT License](https://opensource.org/licenses/MIT)
+- Copyright: Jeconias Santos
+- Status: Desenvolvimento
---
+++ src/CRUD.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * @author     Jeconias Santos <jeconiass2009@hotmail.com>
+ * @license    https://opensource.org/licenses/MIT MIT License
+ * @copyright  Jeconias Santos
+ * @version    v1.0.0
+ *  Você pode utilizar essa class como quiser, contando que mantenha os créditos
+ *  originais em todas as cópias!
+ *
+ *  Ainda irei finalizar os comentários!
+ */
+
+class Crud
+{
+    private $conexao     = null; // CONEXÃO DO BANCO DE DADOS
+    private $DBHost      = null; // HOST PARA CONEXÃO DO BANCO DE DADOS
+    private $DBName      = null; // NOME DO BANCO DE DADOS
+    private $DBUser      = null; // USUÁRIO DO BANCO DE DADOS
+    private $DBPass      = null; // SENHA DO BANCO DE DADOS
+    private $log         = null; // LOG PARA VISUALIZAR QUANDO FOR NECESSÁRIO
+
+    private $Inserido    = null; // QUANTIDADE DE REGISTROS INSERIDOS NA ÚLTIMA QUERY
+    private $Selecionado = null; // DADOS SELECIONADOS DA ÚLTIMA QUERY
+    private $Atualizado  = null; // QUANTIDADE DE REGISTROS ATUALIZADOS NA ÚLTIMA QUERY
+    private $Removido    = null; // QUANTIDADE DE REGISTROS DELETADOS NA ÚLTIMA QUERY
+
+    public function __construct($conexao = null, $config = null)
+    {
+        if ($conexao !== null && $config == null) {
+            $this->conexao = $conexao;
+        } elseif ($conexao == null && $config !== null) {
+            $this->pdo($config[0], $config[1], $config[2], $config[3]);
+        }
+    }
+
+    public function setDBHost($v)
+    {
+        $this->DBHost = $v;
+    }
+    public function setDBName($v)
+    {
+        $this->DBName = $v;
+    }
+    public function setDBUser($v)
+    {
+        $this->DBUser = $v;
+    }
+    public function setDBPass($v)
+    {
+        $this->DBPass = $v;
+    }
+
+    public function setInserir($tabela, $valores, $senha = 'senha')
+    {
+        if (!is_array($valores)) {
+            $this->log .= '<b>Inserindo dados:</b><br>';
+            $this->log .= 'Erro: A variável <b>$valores</b> do método <b>setInserir</b> não é uma array<br>';
+            $this->Inserido = false;
+            return false;
+        }
+        $this->inserir($tabela, $valores, $senha);
+    }
+    public function setSelect($tabela, $valores, $where = null, $limit = null, $order = null)
+    {
+        if (!is_array($valores)) {
+            $this->log .= '<b>Selecionado dados:</b><br>';
+            $this->log .= 'Erro: A variável <b>$valores</b> do método <b>setSelect</b> não é uma array<br>';
+            $this->Selecionado = false;
+            return false;
+        } elseif ($where !== null && !is_array($where)) {
+            $this->log .= '<b>Selecionado dados:</b><br>';
+            $this->log .= 'Erro: A variável <b>$where</b> do método <b>setSelect</b> não é uma array<br>';
+            $this->Selecionado = false;
+            return false;
+        } elseif ($order !== null && !is_array($order)) {
+            $this->log .= '<b>Selecionado dados:</b><br>';
+            $this->log .= 'Erro: A variável <b>$order</b> do método <b>setSelect</b> não é uma array<br>';
+            $this->Selecionado = false;
+            return false;
+        }
+
+        $this->select($tabela, $valores, $where, $limit, $order);
+    }
+    public function setUpdate($tabela, $valores, $where, $senha = 'senha')
+    {
+        if (!is_array($valores)) {
+            $this->log .= '<b>Atualizando dados:</b><br>';
+            $this->log .= 'Erro: A variável <b>$valores</b> do método <b>setUpdate</b> não é uma array<br>';
+            $this->Atualizado = false;
+            return false;
+        } elseif ($where !== null && !is_array($where)) {
+            $this->log .= '<b>Atualizando dados:</b><br>';
+            $this->log .= 'Erro: A variável <b>$where</b> do método <b>setUpdate</b> não é uma array<br>';
+            $this->Atualizado = false;
+            return false;
+        }
+
+        $this->update($tabela, $valores, $where, $senha);
+    }
+    public function setDelete($tabela, $where = null)
+    {
+        if ($where !== null && !is_array($where)) {
+            $this->log .= '<b>Deletando dados:</b><br>';
+            $this->log .= 'Erro: A variável <b>$where</b> do método <b>setDelete</b> não é uma array<br>';
+            $this->Removido = false;
+            return false;
+        }
+
+        $this->Delete($tabela, $where);
+    }
+
+    public function getInserir()
+    {
+        return $this->Inserido;
+    }
+    public function getSelect()
+    {
+        return $this->Selecionado;
+    }
+    public function getUpdate()
+    {
+        return $this->Atualizado;
+    }
+    public function getDelete()
+    {
+        return $this->Removido;
+    }
+
+    public function getLog()
+    {
+        return $this->log;
+    }
+
+    public function run()
+    {
+        $this->pdo($this->DBHost, $this->DBName, $this->DBUser, $this->DBPass);
+    }
+
+    private function pdo($host, $dbname, $dbuser, $dbpass)
+    {
+        try {
+            $pdo = new PDO('mysql:host='.$host.'; dbname='.$dbname, $dbuser, $dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conexao = $pdo;
+            $this->log .= '<br><b>Conexão com o banco de dados:</b><br>';
+            $this->log .= 'Inicializada | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+        } catch (\PDOException $e) {
+            die("Erro de conexão: " . $e->getMessage());
+        }
+    }
+
+    private function inserir($tabela, $fields, $senha)
+    {
+        try {
+            foreach ($fields as $key => $value) {
+                $keys [] = ':'.$key;
+            }
+
+            $first_keys = implode(', ', array_keys($fields));
+            $last_keys = implode(', ', array_values($keys));
+
+            $sql = 'INSERT INTO '.$tabela.' ('.$first_keys.') VALUES ('.$last_keys.')';
+            $query = $this->conexao->prepare($sql);
+
+            foreach ($fields as $key => $value) {
+                if ($key == $senha) {
+                    $query->bindvalue(':'.$key, $this->hash($value));
+                } else {
+                    $query->bindvalue(':'.$key, $value);
+                }
+            }
+            $query->execute();
+            $this->log .= '<b>Inserindo dados:</b><br>';
+            $this->log .= 'Sucesso | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            $this->Inserido = $query->rowCount();
+            return true;
+        } catch (\Exception $e) {
+            $this->log .= '<b>Inserindo dados:</b><br>';
+            $this->log .= 'Erro: '.$e->getMessage().' | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            return false;
+        }
+    }
+
+    private function select($tabela, $valores, $where, $limit, $orderBy)
+    {
+        try {
+            if ($where != null) {
+                $arr_where = $where;
+                $count = count($where);
+                $where = array_keys($where);
+                $where_sql = 'WHERE '.$where[0].'=:'.$where[0];
+
+                $a = 1;
+                while ($a < $count) {
+                    if (!ctype_alpha($where[$a])) {
+                        return false;
+                    }
+                    if (count($where) > 1) {
+                        $where_sql .= ' AND '.$where[$a].'=:'.$where[$a];
+                    }
+                    $a++;
+                }
+            } else {
+                $where_sql = null;
+            }
+
+            if ($limit != null && is_numeric($limit)) {
+                $limit = 'LIMIT '.$limit;
+            } else {
+                $limit = null;
+            }
+
+            if (count($valores) >= 1 && is_array($valores)) {
+                $valores = implode(', ', $valores);
+            }
+
+            if ($orderBy != null && count($orderBy) == 1) {
+                $filter = array_keys($orderBy);
+                $order = array_values($orderBy);
+                $orderBy = 'ORDER BY '.$filter[0].' '.$order[0];
+            } else {
+                $orderBy = null;
+            }
+
+            $sql = 'SELECT '.$valores.' FROM '.$tabela.' '.$where_sql.' '.$orderBy.' '.$limit;
+            $query = $this->conexao->prepare($sql);
+
+            if ($where != null) {
+                foreach ($arr_where as $key => $value) {
+                    $query->bindValue(':'.$key, $value);
+                }
+            }
+            $query->execute();
+            $this->log .= '<b>Selecionando dados:</b><br>';
+            $this->log .= 'Sucesso | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            $this->Selecionado = $query->fetchAll(\PDO::FETCH_ASSOC);
+            return true;
+        } catch (\Exception $e) {
+            $this->log .= '<b>Selecionando dados:</b><br>';
+            $this->log .= 'Erro: '.$e->getMessage().' | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            return false;
+        }
+    }
+
+
+
+    private function update($tabela, $valores, $where, $senha)
+    {
+        try {
+            $key = array_keys($valores);
+            foreach ($key as $value) {
+                $keys [] = $value.'=:'.$value;
+            }
+            $keys = implode(', ', $keys);
+            $arr_where = $where;
+            $count = count($where);
+            $where = array_keys($where);
+            $where_sql = 'WHERE '.$where[0].'=:'.$where[0];
+
+            $a = 1;
+            while ($a < $count) {
+                if (!ctype_alpha($where[$a])) {
+                    return false;
+                }
+                if (count($where) > 1) {
+                    $where_sql .= ' AND '.$where[$a].'=:'.$where[$a];
+                }
+                $a++;
+            }
+
+            $sql = 'UPDATE '.$tabela.' SET '.$keys.' '.$where_sql;
+            $query = $this->conexao->prepare($sql);
+            foreach ($valores as $key => $value) {
+                if ($key == $senha) {
+                    $query->bindvalue(':'.$key, $this->setHash($value));
+                } else {
+                    $query->bindvalue(':'.$key, $value);
+                }
+            }
+            foreach ($arr_where as $key => $value) {
+                $query->bindvalue(':'.$key, $value);
+            }
+            $query->execute();
+            $this->log .= '<b>Inserindo dados:</b><br>';
+            $this->log .= 'Sucesso | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            $this->Atualizado = $query->rowCount();
+            return true;
+        } catch (\Exception $e) {
+            $this->log .= '<b>Atualizando dados:</b><br>';
+            $this->log .= 'Erro: '.$e->getMessage().' | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            return false;
+        }
+    }
+
+    private function Delete($tabela, $where)
+    {
+        try {
+            if ($where != null) {
+                $arr_where = $where;
+                $count = count($where);
+                $where = array_keys($where);
+                $where_sql = ' WHERE '.$where[0].'=:'.$where[0];
+
+                $a = 1;
+                while ($a < $count) {
+                    if (count($where) > 1) {
+                        $where_sql .= ' AND '.$where[$a].'=:'.$where[$a];
+                    }
+                    $a++;
+                }
+            } else {
+                $where_sql = null;
+            }
+
+            $sql = 'DELETE FROM '.$tabela.$where_sql;
+            $query = $this->conexao->prepare($sql);
+
+            if ($where_sql !== null) {
+                foreach ($arr_where as $key => $value) {
+                    $query->bindvalue(':'.$key, $value);
+                }
+            }
+
+            $query->execute();
+            $this->log .= '<b>Inserindo dados:</b><br>';
+            $this->log .= 'Sucesso | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            $this->Removido = $query->rowCount();
+            return true;
+        } catch (\Exception $e) {
+            $this->log .= '<b>Removendo dados:</b><br>';
+            $this->log .= 'Erro: '.$e->getMessage().' | '.$_SERVER['REMOTE_ADDR'].' | '.date('d-m-Y H:i:s').'<br>';
+            return false;
+        }
+    }
+
+    //CRIPTOGRAFIA DE SENHA POR HASH
+    private function salt()
+    {
+        $string = 'abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ0123456789';
+        $retorno = '';
+        for ($i = 1; $i <= 22; $i++) {
+            $rand = mt_rand(1, strlen($string));
+            $retorno .= $string[$rand-1];
+        }
+        return $retorno;
+    }
+
+    private function hash($senha)
+    {
+        return crypt($senha, '$2a$10$' . $this->salt() . '$');
+    }
+}
